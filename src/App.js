f58053bea68d6c5f80287cd7b4c601c1faf72f7f
Lucas Kummer <kummer.lucas@gmail.com>
2021-05-18 08:57:07 +0200

    Adapt logo and change hp route

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Let's <code>vaccinate</code> Köln!
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://termin.corona-impfung.nrw/home"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          register
         </a>
       </header>
     </div>
