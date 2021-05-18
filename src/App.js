82b535c25fd4415f0a23be7784f6a830600140c4
Lucas Kummer <kummer.lucas@gmail.com>
2021-05-18 09:51:38 +0200

    Adapt favicon and change hp route

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h2>#joodjeimpft</h2>
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Let's <code>vaccinate</code> Köln!
